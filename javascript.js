7e488aa42ea358d40037b005f4aab10b55ea71de
yotaadachi <yota961219@gmail.com>
2022-05-04 15:05:23 +0900

    constとスコープ

--- javascript.js
+++ javascript.js
@@ -1,7 +1,21 @@
-let value = 2;
+const value = 1;
+
 console.log(value);
 
-if(value === 2) {
-  value = 20;
+if(true) {
+  const value = 2;
+  console.log(value);
 }
 console.log(value);
+
+const profile = {
+  age: 45,
+  name: 'Ham',
+};
+
+console.log(profile);
+
+//profile.gender = 'male';
+profile['gender'] = 'female';
+
+console.log(profile);
