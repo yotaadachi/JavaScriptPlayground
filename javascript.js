6b21534a9580626760504cd8ceef88fb916fccc9
yotaadachi <yota961219@gmail.com>
2022-05-05 15:43:03 +0900

    非同期処理を行う上で使用するコールバックの弱点とは?

--- javascript.js
+++ javascript.js
@@ -1,17 +1,11 @@
-const request = new XMLHttpRequest();
 console.log(1);
-request.open('GET', 'https://jsonplaceholder.typicode.com/photos', true);
-console.log(2);
-request.onload = function() {
-  console.log(3);
-  //request.status のチェックをここに移動する。
-  if(request.status === 200) {
-    console.log(request.response);
-  }
-}
-const before = Date.now();
-request.send(null);
-const after = Date.now();
-const ms = after - before;
-console.log(`${ms}: ms`);
-console.log(4);
+setTimeout(() => {
+  console.log('1秒たったよ');
+  setTimeout(() => {
+    console.log('1秒たったよ');
+    setTimeout(() => {
+      console.log('1秒たったよ');
+    }, 1000);
+  }, 1000);
+}, 1000);
+console.log(3);
