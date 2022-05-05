089abccaf626b0f2f2541c76e1cc059e973bca97
yotaadachi <yota961219@gmail.com>
2022-05-05 14:35:10 +0900

    mapメソッドを使ってみよう

--- javascript.js
+++ javascript.js
@@ -1,12 +1,6 @@
-const numbers = [1,2,3];
-const integers = [4,5,6];
-console.log(numbers.concat(integers));
-console.log([...numbers,...integers]);
-
-const profile1 = { nickname: 'Ham' };
-const profile2 = { age: 45 };
-
-const profile = {...profile1, ...profile2};
-console.log(profile);
-console.log(profile1);
-console.log(profile2);
+const numbers = [5,3,4];
+const square = (number) => {
+  return number * number;
+}
+const squares = numbers.map(square);
+console.log(squares);
