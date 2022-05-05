5965a3ae335705bf53be918194c1b8b9faceb010
yotaadachi <yota961219@gmail.com>
2022-05-05 11:39:07 +0900

    機能を再利用するためのimportとexprotについて学ぼう

--- javascript.js
+++ javascript.js
@@ -1,11 +1,2 @@
-
-const square = length => length * length;
-
-console.log(square(4));
-
-function foo() {
-  return true;
-}
-
-console.log(foo());
-console.log(foo);
+import profile from './profile.js';
+console.log(profile);
---
+++ profile.js
@@ -0,0 +1,9 @@
+export const name = 'Ham';
+export const age = 45;
+
+const profile = {
+  age: 45,
+  name: 'Ham',
+};
+
+export default profile;
