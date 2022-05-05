13210589993be41fa2f833cc03001bb4e12f1968
yotaadachi <yota961219@gmail.com>
2022-05-05 14:03:24 +0900

    オブジェクトの分割代入をやってみよう

--- javascript.js
+++ javascript.js
@@ -1,6 +1,14 @@
-const array = ['Ham', 45, 'EggPlants', 'Oranges'];
-const [nickname, age, ...rest] = array;
+const profile = {
+  nickname: 'Ham',
+  age: 45
+}
 
+const { nickname, ...rest } = profile;
 console.log(nickname);
-console.log(age);
 console.log(rest);
+
+const printObject = ({nickname}) => {
+  console.log(`nickname: ${nickname}`)
+};
+
+printObject(profile);
