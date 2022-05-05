08d88026fb347e47a21a772e58de4d8fa974bac3
yotaadachi <yota961219@gmail.com>
2022-05-05 15:23:57 +0900

    非同期処理とは何か?

--- javascript.js
+++ javascript.js
@@ -1,6 +1,17 @@
-const numbers = [5,3,4];
-const square = (number) => {
-  return number * number;
+const request = new XMLHttpRequest();
+console.log(1);
+request.open('GET', 'https://jsonplaceholder.typicode.com/photos', true);
+console.log(2);
+request.onload = function() {
+  console.log(3);
+  //request.status のチェックをここに移動する。
+  if(request.status === 200) {
+    console.log(request.response);
+  }
 }
-const squares = numbers.map(square);
-console.log(squares);
+const before = Date.now();
+request.send(null);
+const after = Date.now();
+const ms = after - before;
+console.log(`${ms}: ms`);
+console.log(4);
