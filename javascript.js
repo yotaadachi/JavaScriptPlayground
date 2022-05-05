acac71037171c5e89ce9a38f6ce527f87d07636f
yotaadachi <yota961219@gmail.com>
2022-05-05 16:16:15 +0900

    プロミスにsetTimeoutを使ってみよう

--- javascript.js
+++ javascript.js
@@ -1,20 +1,26 @@
 const promise = new Promise((resolve, reject) => {
   // 時間のかかる処理
   // resolve();
-  console.log(resolve);
-  console.log(reject);
-});
-console.log(promise);
-console.log('start');
-
-setTimeout(() => {
-  console.log('1秒たったよ');
   setTimeout(() => {
     console.log('1秒たったよ');
-    setTimeout(() => {
-      console.log('1秒たったよ');
-    }, 1000);
+    resolve();
   }, 1000);
-}, 1000);
+});
+console.log(promise);
+
+promise.then(() => {
+  console.log('I am called by then method');
+});
+console.log('start');
+
+// setTimeout(() => {
+//   console.log('1秒たったよ');
+//   setTimeout(() => {
+//     console.log('1秒たったよ');
+//     setTimeout(() => {
+//       console.log('1秒たったよ');
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
 
 console.log('end');
