08100c83c01d92f2221f4589d2208b17682ee513
yotaadachi <yota961219@gmail.com>
2022-05-07 14:37:41 +0900

    asyncとawaitを使ってfetchを使ってみよう

--- javascript.js
+++ javascript.js
@@ -1,11 +1,10 @@
-const result = fetch('https://jsonplaceholder.typicode.com/todos/1')
-console.log({result});
+const url = 'https://jsonplaceholder.typicode.com/users';
 
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then((response) => {
-    console.log({response});
-    const json = response.json();
-    console.log({json});
-    return json;
-  })
-  .then((json) => console.log(json));
+const fetchUsers = async () => {
+  const result = await fetch(url);
+  console.log({ result });
+  const json = result.json();
+  console.log({ json });
+};
+
+fetchUsers();
