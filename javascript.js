b8d3b20eb74e9c6700a5badb8519f3c0085229fc
yotaadachi <yota961219@gmail.com>
2022-05-07 14:26:21 +0900

    外部サーバと通信するためのfetchAPIを使ってみよう

--- javascript.js
+++ javascript.js
@@ -1,30 +1,11 @@
-console.log('start');
+const result = fetch('https://jsonplaceholder.typicode.com/todos/1')
+console.log({result});
 
-const sleep = (milliseconds) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log(`${milliseconds} ms`);
-      resolve();
-    }, milliseconds);
-  });
-};
-
-// sleep(900)
-//   .then(() => sleep(901))
-//   .then(() => sleep(902))
-//   .then(() => sleep(903))
-//   .then(() => sleep(904))
-//   .then(() => sleep(905))
-//   .then(() => sleep(906));
-
-async function asyncCall() {
-  await sleep(900);
-  await sleep(901);
-  await sleep(902);
-  await sleep(903);
-  await sleep(904);
-}
-
-asyncCall();
-
-console.log('end');
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then((response) => {
+    console.log({response});
+    const json = response.json();
+    console.log({json});
+    return json;
+  })
+  .then((json) => console.log(json));
