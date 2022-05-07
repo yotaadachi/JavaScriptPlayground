cc114ae6dfb6e684cbb9ba5fffd405ed2e2c9e13
yotaadachi <yota961219@gmail.com>
2022-05-07 14:49:27 +0900

    同期処理と非同期処理を正しく分類しよう

--- javascript.js
+++ javascript.js
@@ -3,11 +3,11 @@ setTimeout(() => console.log(1));
 queueMicrotask(() => console.log(2));
 
 new Promise((resolve) => {
-  console.log(3);
+  // console.log(3);
   setTimeout(() => console.log(4));
   resolve();
 }).then(() => {
   console.log(5);
 });
 
-console.log(6);
+// console.log(6);
