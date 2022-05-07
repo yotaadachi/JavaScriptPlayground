b17858ed15ef890ddb88a1505b226372927a81f9
yotaadachi <yota961219@gmail.com>
2022-05-07 12:41:45 +0900

    asyncとawaitについて

--- javascript.js
+++ javascript.js
@@ -9,12 +9,22 @@ const sleep = (milliseconds) => {
   });
 };
 
-sleep(900)
-  .then(() => sleep(901))
-  .then(() => sleep(902))
-  .then(() => sleep(903))
-  .then(() => sleep(904))
-  .then(() => sleep(905))
-  .then(() => sleep(906));
+// sleep(900)
+//   .then(() => sleep(901))
+//   .then(() => sleep(902))
+//   .then(() => sleep(903))
+//   .then(() => sleep(904))
+//   .then(() => sleep(905))
+//   .then(() => sleep(906));
+
+async function asyncCall() {
+  await sleep(900);
+  await sleep(901);
+  await sleep(902);
+  await sleep(903);
+  await sleep(904);
+}
+
+asyncCall();
 
 console.log('end');
